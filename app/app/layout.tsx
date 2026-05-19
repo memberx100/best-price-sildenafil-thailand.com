54d3de4d685a71da98cc16bd75e19ba71a6f69ca
memberx100 <memberx100@protonmail.com>
2026-05-20 00:46:41 +0200

    Update layout.tsx

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Best Price Sildenafil Thailand",
+  description: "ED medication information and price comparison in Thailand.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
